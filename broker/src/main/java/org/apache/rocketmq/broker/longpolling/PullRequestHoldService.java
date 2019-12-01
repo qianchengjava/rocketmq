410df6d15f37e4b56d71181b3fa6c7a9c6a40101
qiancheng <qiancheng@shuidihuzhu.com>
2019-12-01 15:26:34 +0800

    aaa

--- broker/src/main/java/org/apache/rocketmq/broker/longpolling/PullRequestHoldService.java
+++ broker/src/main/java/org/apache/rocketmq/broker/longpolling/PullRequestHoldService.java
@@ -43,6 +43,7 @@ public class PullRequestHoldService extends ServiceThread {
 
     public void suspendPullRequest(final String topic, final int queueId, final PullRequest pullRequest) {
         String key = this.buildKey(topic, queueId);
+
         ManyPullRequest mpr = this.pullRequestTable.get(key);
         if (null == mpr) {
             mpr = new ManyPullRequest();
